8f9d0a07d2d46ddf839f753883872aeb4a2cbe9e
Imonikai <108214765+Imonikai@users.noreply.github.com>
2023-09-10 00:30:11 +0900

    XInputクラスを追加

---
+++ DxLibProject/XInput.cpp
@@ -0,0 +1,123 @@
+﻿#include "XInput.h"
+#include <climits>
+
+XINPUT_STATE XInput::padState;
+int XInput::padNumber = 1;
+unsigned char XInput::triggerActionPoint = 0;
+float XInput::deadZone = 0.35f;
+float XInput::leftStickX;
+float XInput::leftStickY;
+float XInput::rightStickX;
+float XInput::rightStickY;
+int XInput::inputs[MY_XINPUT_INPUT_NUM] = { 0 };
+
+void XInput::updateStick()
+{
+    /*スティックの入力を正規化*/
+    leftStickX = padState.ThumbLX / 32767.0f;
+    leftStickY = padState.ThumbLY / 32767.0f;
+    rightStickX = padState.ThumbRX / 32767.0f;
+    rightStickY = padState.ThumbRY / 32767.0f;
+
+    /*デッドゾーンを考慮して入力を確定する*/
+    if (leftStickX < deadZone && leftStickX > -deadZone) leftStickX = 0.0f;
+    if (leftStickY < deadZone && leftStickY > -deadZone) leftStickY = 0.0f;
+    if (rightStickX < deadZone && rightStickX > -deadZone) rightStickX = 0.0f;
+    if (rightStickY < deadZone && rightStickY > -deadZone) rightStickY = 0.0f;
+
+    /*inputs配列を加算する*/
+    /*左スティック*/
+    if (leftStickX < -deadZone) inputs[MY_XINPUT_LEFT_STICK_LEFT]++;
+    else inputs[MY_XINPUT_LEFT_STICK_LEFT] = 0;
+    if (leftStickX > deadZone) inputs[MY_XINPUT_LEFT_STICK_RIGHT]++;
+    else inputs[MY_XINPUT_LEFT_STICK_RIGHT] = 0;
+    if (leftStickY > deadZone) inputs[MY_XINPUT_LEFT_STICK_UP]++;
+    else inputs[MY_XINPUT_LEFT_STICK_UP] = 0;
+    if (leftStickY < -deadZone) inputs[MY_XINPUT_LEFT_STICK_DOWN]++;
+    else inputs[MY_XINPUT_LEFT_STICK_DOWN] = 0;
+    /*右スティック*/
+    if (rightStickX < -deadZone) inputs[MY_XINPUT_RIGHT_STICK_LEFT]++;
+    else inputs[MY_XINPUT_RIGHT_STICK_LEFT] = 0;
+    if (rightStickX > deadZone) inputs[MY_XINPUT_RIGHT_STICK_RIGHT]++;
+    else inputs[MY_XINPUT_RIGHT_STICK_RIGHT] = 0;
+    if (rightStickY > deadZone) inputs[MY_XINPUT_RIGHT_STICK_UP]++;
+    else inputs[MY_XINPUT_RIGHT_STICK_UP] = 0;
+    if (rightStickY < -deadZone) inputs[MY_XINPUT_RIGHT_STICK_DOWN]++;
+    else inputs[MY_XINPUT_RIGHT_STICK_DOWN] = 0;
+}
+
+void XInput::updateTrigger()
+{
+    if (padState.LeftTrigger > triggerActionPoint)
+        inputs[MY_XINPUT_LEFT_TRIGGER]++;
+    else inputs[MY_XINPUT_LEFT_TRIGGER] = 0;
+    if (padState.RightTrigger > triggerActionPoint)
+        inputs[MY_XINPUT_RIGHT_TRIGGER]++;
+    else inputs[MY_XINPUT_RIGHT_TRIGGER] = 0;
+}
+
+
+BOOL XInput::init(const int _padNumber)
+{
+    if (_padNumber < 1 || _padNumber > 4) padNumber = 1;
+
+    return update();
+}
+
+BOOL XInput::update()
+{
+
+    int result = GetJoypadXInputState(padNumber, &padState);
+    if (result == 0)
+    {
+        /*XINPUT_STATE構造体のButtonsの範囲をinputsに反映させる*/
+        for (int i = 0; i < 16; i++)
+        {
+            if (padState.Buttons[i] == 1)
+            {
+                inputs[i]++;
+            }
+            else
+            {
+                inputs[i] = 0;
+            }
+        }
+        /*スティックとトリガーをinputsに反映*/
+        updateStick();
+        updateTrigger();
+
+        /*inputsのint範囲超え対策*/
+        for (int i = 0; i < MY_XINPUT_INPUT_NUM; i++)
+        {
+            inputs[i] %= INT_MAX;
+        }
+    }
+    /*デバッグ*/
+    /*
+    clsDx();
+    printfDx(_T("leftStickX = % f, leftStickY = % f\n"), leftStickX, leftStickY);
+    printfDx(_T("rightStickX = %f, rightStickY = %f\n"), rightStickX, rightStickY);
+    printfDx(_T("leftTrigger = %d rightTrigger = %d\n"), padState.LeftTrigger, padState.RightTrigger);
+    for (int i = 0; i < MY_XINPUT_INPUT_NUM; i++)
+    {
+        printfDx(_T("inputs[%d] = %d\n"), i, inputs[i]);
+    }
+    */
+    return TRUE;
+}
+
+int XInput::getInputHit(const unsigned char inputCode)
+{
+    if (inputCode < 0 || inputCode > MY_XINPUT_INPUT_NUM) return -1;
+    return inputs[inputCode];
+}
+
+void XInput::setDeadZone(const float newDeadZone)
+{
+    deadZone = newDeadZone;
+}
+
+void XInput::setTriggerActionPoint(const unsigned char point)
+{
+    triggerActionPoint = point;
+}
---
+++ DxLibProject/XInput.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "DxLib.h"
+
+/*XINPUTの入力取得のための追加定数（DXライブラリの仕様変更があるかもしれないのでMY_をつけておく)*/
+#define MY_XINPUT_LEFT_STICK_UP 16
+#define MY_XINPUT_LEFT_STICK_DOWN 17
+#define MY_XINPUT_LEFT_STICK_LEFT 18
+#define MY_XINPUT_LEFT_STICK_RIGHT 19
+#define MY_XINPUT_RIGHT_STICK_UP 20
+#define MY_XINPUT_RIGHT_STICK_DOWN 21
+#define MY_XINPUT_RIGHT_STICK_LEFT 22
+#define MY_XINPUT_RIGHT_STICK_RIGHT 23
+#define MY_XINPUT_LEFT_TRIGGER 24
+#define MY_XINPUT_RIGHT_TRIGGER 25
+#define MY_XINPUT_INPUT_NUM 26
+
+
+class XInput
+{
+private:
+
+    /* パッドの情報更新用変数 */
+    static XINPUT_STATE padState; //パッドの情報を取得する構造体
+    static int padNumber; //パッドの番号
+    static unsigned char triggerActionPoint; //トリガーを反応させる値
+    static float deadZone; //スティックのデッドゾーン -1.0 ~ 1.0
+    static float leftStickX; //正規化された左スティックのx -1.0 ~ 1.0
+    static float leftStickY; //正規化された左スティックのy -1.0 ~ 1.0
+    static float rightStickX; //正規化された右スティックのx -1.0 ~ 1.0
+    static float rightStickY; //正規化された右スティックのy -1.0 ~ 1.0
+
+    /* パッドの情報蓄積用変数（KeyInutと同じような感じで使うための変数) */
+    static int inputs[];
+
+
+    static void updateStick();
+    static void updateTrigger();
+    XInput() = delete;
+
+public:
+
+    static BOOL init(const int _padNumber);
+    static BOOL update();
+    static int getInputHit(const unsigned char inputCode);
+    static void setDeadZone(const float newDeadZone);
+    static void setTriggerActionPoint(const unsigned char point);
+
+};
+

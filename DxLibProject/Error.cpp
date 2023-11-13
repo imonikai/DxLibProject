cdd3f4e7ce1d2f0fd6dd937761352c57ff4658b9
imonikai <108214765+imonikai@users.noreply.github.com>
2023-11-13 20:19:19 +0900

    エラーメッセージ小さくないか問題修正

--- DxLibProject/Error.cpp
+++ DxLibProject/Error.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Error.h"
 #include "KeyInput.h"
+#include "define.h"
 
 void ErrorEnd() {
     DxLib_End();
@@ -11,13 +12,15 @@ void ErrorMessageBoxOK(const TCHAR* message) {
 }
 
 void FinishWithError(const TCHAR* message, const TCHAR* functionName, int line) {
-    printfDx(_T("異常が発生しました。\n%s\n%s(%d)\nEscキーを押してソフトを終了します。")
-        , message
-        , functionName
-        , line
-    );
+    SetFontSize((int)(GRAPH_WIDTH / 30));
     while (!ProcessMessage()) {
         ClearDrawScreen();
+        DrawFormatString(0, 0, GetColor(255, 255, 255),
+            _T("異常が発生しました。\nmsg: %s\nat: %s(%d)\nEscキーを押してソフトを終了します。")
+            , message
+            , functionName
+            , line
+        );
         ScreenFlip();
         KeyInput::update();
         if (KeyInput::getKeyHit(KEY_INPUT_ESCAPE))
